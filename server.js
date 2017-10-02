23745055a244ee4254e29c2a6f78e8b2f9f75b6d
Nathan Moore <ncmoore1986@gmail.com>
2017-10-02 09:22:13 -0700

    further basic set up stuff

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const EXPRESS = require('express');
+const bodyParser = require('body-parser');
+const PG = require('pg');
+const PORT = process.env.PORT || 3000;
+const app = EXPRESS();
