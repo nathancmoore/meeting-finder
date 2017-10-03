e24bf1537f7bf66bfe4b4cd62ed2306d405790ef
Matt Favoino <mattfavoino@gmail.com>
2017-10-03 13:27:36 -0700

    A map is visble. Nathan is excited

--- public/scripts/views/mapview.js
+++ public/scripts/views/mapview.js
@@ -1,10 +1,46 @@
 'use strict';
 
-// var app = app || {};
-//
-// var geocoder;
-// var map;
-//
+
+var map;
+var infoWindow;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('google-map'), {
+    center: {lat: 47.608, lng: -122.335167},
+    zoom: 10
+  });
+  infoWindow = new google.maps.InfoWindow;
+
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
+
+
+
+
+
 // function initMap() {
 //   geocoder = new google.maps.Geocoder();
 //   var latlng = new google.maps.LatLng(47.608, -122.335167);
