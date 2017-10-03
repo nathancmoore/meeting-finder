33769d2d67867feaf178eb22a11c6c83ebb70f27
CHELSEA DOLE <chelseadole@gmail.com>
2017-10-03 14:47:11 -0700

    fixing calendar, search, radius, and time functions within FormModel function, and adding corresponding segments in index.html

--- public/scripts/model/FormModel.js
+++ public/scripts/model/FormModel.js
@@ -4,44 +4,40 @@ var app = app || {};
 
 (function (module) {
 
-// FIXME: make sure that the parameter/argument being passed through here is the one that matches the data object being returned by server. 
+// FIXME: make sure that the parameter/argument being passed through here is the one that matches the data object being returned by server.
   // function FormData(rawDataObj) {
   //   Object.keys(rawDataObj).forEach(key => this[key] = rawDataObj[key]);
   // }
 
   FormData.all = [];
-  var calendarString 
-  
-  let $searchForm = $('#input-location');
-  $searchForm.on('keypress', function(event){
-    event.preventDefault();
-    let searchString = $searchForm.val().trim();
+  var calendarString;
+  var searchString;
+  var radiusString;
+  var timeString;
 
-    let filteredOut = $('#input-location').toArray().filter(function(row){
-      return !row.id.includes(searchString);
-    });
-    $(filteredIn).show();
-    $(filteredIn).each((idx, item) => {
-      $(item).next().show()
-    });
-    $(filteredOut).hide();
-    $(filteredOut).each((idx, item) => {
-      $(item).next().hide()
-    });
-  })
-  //TODO: create form on table with criteria to search with
+  let $searchForm = $('#input-location');
+  $searchForm.on('submit', function(){
+    searchString = $searchForm.val();
+    console.log(searchString);
+  });
+
   let $calendarData = $('#input-date');
-  $calendarData.on('click', function() {
-    // event.preventDefault();
+  $calendarData.on('submit', function() {
     calendarString = $calendarData.val();
     console.log(calendarString);
-  }); 
+  });
 
-  //TODO:  filter out the meetings that are not submitted into the form as the "meet this criteria fields" 
+  let $timeData = $('#input-time');
+  $timeData.on('submit', function() {
+    timeString = $timeData.val();
+    console.log(timeString);
+  });
 
-  //TODO: Take the filtered list of meetings and route the data to the map population route/controller. 
-
-  //TODO: Take the filtered list and route the data to the static list route/controller. 
+  let $radiusData = $('#input-radius');
+  $radiusData.on('submit', function() {
+    radiusString = $radiusData.val();
+    console.log(radiusString);
+  });
 
   module.FormData = FormData;
 })(app);
