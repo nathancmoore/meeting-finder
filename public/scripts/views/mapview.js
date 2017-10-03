4e3bf248aec873c542533f1e5665d266eccbb646
Matt Favoino <mattfavoino@gmail.com>
2017-10-03 14:01:53 -0700

    We added a marker. Nathan is smart

--- public/scripts/views/mapview.js
+++ public/scripts/views/mapview.js
@@ -1,13 +1,15 @@
 'use strict';
 
-
+var userLocation;
 var map;
 var infoWindow;
+var geocoder;
 
 function initMap() {
+  geocoder = new google.maps.Geocoder();
   map = new google.maps.Map(document.getElementById('google-map'), {
     center: {lat: 47.608, lng: -122.335167},
-    zoom: 10
+    zoom: 6
   });
   infoWindow = new google.maps.InfoWindow;
 
@@ -17,9 +19,10 @@ function initMap() {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
+      userLocation = pos;
 
       infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
+      infoWindow.setContent('User Location');
       infoWindow.open(map);
       map.setCenter(pos);
     }, function() {
@@ -37,31 +40,15 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.open(map);
 }
 
-
-
-
-
-// function initMap() {
-//   geocoder = new google.maps.Geocoder();
-//   var latlng = new google.maps.LatLng(47.608, -122.335167);
-//   var mapSetup = {
-//     center: latlng,
-//     zoom: 10
-//   };
-//   map = new google.maps.Map(document.getElementById('google-map'), mapSetup);
-// }
-//
-// function codeAddress(address) {
-//   geocoder.geocode( {'address': address}, function(results, status) {
-//     if(status === google.maps.GeocoderStatus.OK) {
-//       map.setCenter(results[0].geometry.location);
-//       var marker = new google.maps.Marker({
-//         map: map,
-//         position: results[0].geometry.location
-//       });
-//     } else {
-//       console.log('status');
-//     }
-//   }
-// )
-// };
+function makeMarkers() {
+  app.meetings.timeTarget.forEach(ele => {
+    geocoder.geocode( { 'address': ele.street}, function(results, status) {
+      if (status === 'OK') {
+        var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+        });
+      }
+    }
+    )})
+}
