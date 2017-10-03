fc3e46c334816ab86dbf31bd124bd5d1f6141560
Matt Favoino <mattfavoino@gmail.com>
2017-10-02 19:24:59 -0700

    Building initMap and codeAddress functions and added api script

--- public/scripts/views/mapview.js
+++ public/scripts/views/mapview.js
@@ -2,10 +2,30 @@
 
 var app = app || {};
 
+var geocoder;
+var map;
+
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    //lat and lng below will need actual geocoded variables
-    center: { lat: Geocoded_input lng: Geocoded_input},
-    zoom: 16;
-  });
-}
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(47.608, -122.335167);
+  var mapSetup = {
+    center: latlng,
+    zoom: 10
+  }
+  map = new google.maps.Map(document.getElementById('google-map'), mapSetup);
+};
+
+function codeAddress(address) {
+  geocoder.geocode( {'address': address}, function(results, status) {
+    if(status === google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location
+      });
+    } else {
+      console.log('status');
+    }
+  }
+  )
+};
