53e6981fc4465bbb45c87a041cfffb4dbd16fb5f
Jacob M-G Evans <jacobmgevans@gmail.com>
2017-10-03 11:19:23 -0700

    changed port to 3000, and more index changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const EXPRESS = require('express');
 const bodyParser = require('body-parser');
 const PG = require('pg');
 const FS = require('fs');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = EXPRESS();
 
 const conString = process.env.DATABASE_URL || 'postgres://postgres:postgresPASSWORD123@localhost:5432/meetingfinder';
