db70b7481e40c367bb145128ce193618fb099de1
Jacob M-G Evans <jacobmgevans@gmail.com>
2017-10-03 15:33:42 -0700

    Chelsea fixed the form logic

--- public/scripts/views/FormView.js
+++ public/scripts/views/FormView.js
@@ -4,19 +4,11 @@ var app = app || {};
 
 (function (module) {
 
-// FIXME: make sure that the parameter/argument being passed through here is the one that matches the data object being returned by server.
-  // function FormData(rawDataObj) {
-  //   Object.keys(rawDataObj).forEach(key => this[key] = rawDataObj[key]);
-  // }
-
   var Submission = {};
-
-  FormData.all = [];
 
   $('#form').on('submit', function(event) {
     event.preventDefault();
 
-    Submission.searchString = $('#input-location').val();
     Submission.calendarString = $('#input-date').val();
     Submission.timeString = $('#input-time').val();
     Submission.radiusString = $('#input-radius').val();
