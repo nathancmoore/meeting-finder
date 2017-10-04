cfb383e6b38ce7405df16c6a2eb1582a41c81214
CHELSEA DOLE <chelseadole@gmail.com>
2017-10-04 16:11:45 -0700

    finishing integrating with filtered file

--- public/scripts/views/meetingTableView.js
+++ public/scripts/views/meetingTableView.js
@@ -4,27 +4,16 @@ var app = app || {};
 
 (function (module) {
 
-  console.log('in IIFE');
-
   //array of all objects being appended to page (after time/date and location filtering)
 
   const tableView = {};
-
-  tableView.tempData = app.meetings.all;
-
-  // tableView.tempData.sort(function(meetObj1, meetObj2) {
-  //   return meetObj1.Meet.time - meetObj2.Meet.time;
-  // })
 
   tableView.sourceHTML = $('#meeting-template').html();
   tableView.meetTemplate = Handlebars.compile(tableView.sourceHTML);
 
   tableView.populateTable = function() {
-    console.log('in populateTable');
-    console.log(tableView.tempData[0])
-    tableView.tempData.forEach(function(meet) {
-      console.log('in forEach');
-      var newRawHTML = tableView.meetTemplate(this);
+    app.meetings.filtered.forEach(function(meet) {
+      var newRawHTML = tableView.meetTemplate(meet);
       $('#meetings-table').append(newRawHTML);
     })
   }
